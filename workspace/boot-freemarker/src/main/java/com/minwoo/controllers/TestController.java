cab2e50e1aa3a6402002d776d9ad197eaeb4e1ee
MinwooKim <minwoo.kim01@lotte.net>
2020-01-16 17:20:03 +0900

    파라메터 변경 시도

--- workspace/boot-freemarker/src/main/java/com/minwoo/controllers/TestController.java
+++ workspace/boot-freemarker/src/main/java/com/minwoo/controllers/TestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class TestController {
     @RequestMapping("/welcome")
-    public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
+    public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="unknown") String name) {
         model.addAttribute("name", name);
         return "welcome";
     }
